0b0190706860134b468af0ab79c6ac2b160bddd9
estebaanfig02 <estebanfig29@gmail.com>
2023-09-15 23:00:56 -0300

    se modifico los archivo calculadora.js, quepersonaje.js, starWars.json

--- calculadora.js
+++ calculadora.js
@@ -4,3 +4,4 @@ let suma1 = (numero1, numero2) => numero1 + numero2 ;
 let resta1 =	(numeroA, numeroB) => numeroA - numeroB ;
 let multiplicacion1 = (numeroA, numeroB) => numeroA * numeroB ;
 let division1 = ( numeroA, numeroB) => numeroA / numeroB ;
+let calculadora1 = (numeroA, numeroB, operacion) => operacion (numeroA, numeroB)
