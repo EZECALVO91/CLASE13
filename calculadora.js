83053d377ea34902a12f98a7f11c388c75798e80
EZECALVO91 <ezequielcalvo@live.com>
2023-09-15 22:43:32 -0300

    SE AGREGARON 3 ARCHIVOS, 1 = json, 1 = txt , 1 = js

---
+++ calculadora.js
@@ -0,0 +1,6 @@
+//CALCULADORA CON FUNCIONES ARROW Y CALLBACKS
+
+let suma1 = (numero1, numero2) => numero1 + numero2 ;
+let resta1 =	(numeroA, numeroB) => numeroA - numeroB ;
+let multiplicacion1 = (numeroA, numeroB) => numeroA * numeroB ;
+let division1 = ( numeroA, numeroB) => numeroA / numeroB ;
